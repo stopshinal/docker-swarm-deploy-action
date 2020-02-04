154593093db47c6c95b2ae32dedc5f09d6c7f3e7
Justin Sinichko <justin.sinichko@gmail.com>
2020-02-03 18:16:28 -0700

    back out all ssh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,40 +7,40 @@ if [ -z "$DOCKER_REMOTE_HOST" ]; then
 fi
 
 # Extra handling for SSH-based connections.
-if [ ${DOCKER_REMOTE_HOST#"ssh://"} != "$DOCKER_REMOTE_HOST" ]; then
-    SSH_HOST=${DOCKER_REMOTE_HOST#"ssh://"}
-    SSH_HOST=${SSH_HOST#*@}
+##if [ ${DOCKER_REMOTE_HOST#"ssh://"} != "$DOCKER_REMOTE_HOST" ]; then
+##    SSH_HOST=${DOCKER_REMOTE_HOST#"ssh://"}
+##    SSH_HOST=${SSH_HOST#*@}
 
-    if [ -z "$DOCKER_SSH_PRIVATE_KEY" ]; then
-        echo "DOCKER_SSH_PRIVATE_KEY is required for SSH hosts!"
-        exit 1
-    fi
+##    if [ -z "$DOCKER_SSH_PRIVATE_KEY" ]; then
+##        echo "DOCKER_SSH_PRIVATE_KEY is required for SSH hosts!"
+##        exit 1
+##    fi
 
-    if [ -z "$DOCKER_SSH_PUBLIC_KEY" ]; then
-        echo "DOCKER_SSH_PUBLIC_KEY is required for SSH hosts!"
-        exit 1
-    fi
+##    if [ -z "$DOCKER_SSH_PUBLIC_KEY" ]; then
+##        echo "DOCKER_SSH_PUBLIC_KEY is required for SSH hosts!"
+##        exit 1
+##    fi
 
-    echo "Registering SSH keys..."
+    ##echo "Registering SSH keys..."
 
     # Save private key to a file and register it with the agent.
-    mkdir -p "$HOME/.ssh"
-    echo "Made directory"
-    printf '%s' "$DOCKER_SSH_PRIVATE_KEY" > "$HOME/.ssh/docker"
-    echo "proof"
-    head -c 50 $HOME/.ssh/docker
-    echo "Added private key"
-    chmod 600 "$HOME/.ssh/docker"
-    echo "Updated chmod"
-    eval $(ssh-agent)
-    echo "Evaled SSH-AGENT"
-    ssh-add "$HOME/.ssh/docker"
-    echo "SSH-Added"
+    ##mkdir -p "$HOME/.ssh"
+    ##echo "Made directory"
+    ##printf '%s' "$DOCKER_SSH_PRIVATE_KEY" > "$HOME/.ssh/docker"
+    ##echo "proof"
+    ##head -c 50 $HOME/.ssh/docker
+    ##echo "Added private key"
+    ##chmod 600 "$HOME/.ssh/docker"
+    ##echo "Updated chmod"
+    ##eval $(ssh-agent)
+    ##echo "Evaled SSH-AGENT"
+    ##ssh-add "$HOME/.ssh/docker"
+    ##echo "SSH-Added"
 
     # Add public key to known hosts.
-    printf '%s %s\n' "$SSH_HOST" "$DOCKER_SSH_PUBLIC_KEY" >> /etc/ssh/ssh_known_hosts
-    echo "Added key to known hosts"
-fi
+    ##printf '%s %s\n' "$SSH_HOST" "$DOCKER_SSH_PUBLIC_KEY" >> /etc/ssh/ssh_known_hosts
+    ##echo "Added key to known hosts"
+##fi
 
 echo "Connecting to $DOCKER_REMOTE_HOST..."
 docker --log-level debug --host "$DOCKER_REMOTE_HOST" "$@" 2>&1
